be78d91c5bae6c522e0549ebaa2ef24ce5066711
Matt <50344355+mkrell23@users.noreply.github.com>
2025-02-27 11:11:10 -0500

    Solves exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    for (const number of toRemove) {
+        while(array.indexOf(number) >= 0 ) {
+            array.splice(array.indexOf(number), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
