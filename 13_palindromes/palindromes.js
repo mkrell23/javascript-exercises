36f0bfc3e6e30adffb79a1a462a81e2abfa2c603
Matt <50344355+mkrell23@users.noreply.github.com>
2025-05-14 16:00:11 -0400

    exercise 13 complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+   let stripped = string.toLowerCase().replace(/[., \/#!$%\^&\*;:{}=\-_`~()]/g,"");
+   let start = 0;
+   let end = stripped.length - 1;
+   for (let i = 0; i < end; i ++){
+    if (stripped[i] != stripped[end]){
+        return false
+    }
+    end --;
+   }
+   return true;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
