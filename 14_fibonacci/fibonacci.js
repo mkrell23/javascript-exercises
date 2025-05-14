b77a58c35bef3b2eda9711076a4f735a0ce47c79
Matt <50344355+mkrell23@users.noreply.github.com>
2025-05-14 16:00:18 -0400

    exercise 14 complete

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(end) {
+    if (parseInt(end) >= 0){
+        let total = 0;
+        let next = 1;
+        for (i = 0; i < end; i++){
+            let temp = total;
+            total = next;
+            next = total + temp;
+        }
+    
+        return total;
+    }
+    else{
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
